18f66f3635e0a02a8274c5f1f9d60184b8321a8e
Ceriole <richie0415@hotmail.com>
2026-05-01 02:15:51 -0400

    Updated version label (explicity post-jam)

--- screens/main_menu.gd
+++ screens/main_menu.gd
@@ -5,7 +5,7 @@ extends Control
 @export_file("*.tscn") var game_scene_path: String
 @export_file("*.tscn") var options_scene_path: String
 
-@onready var version_label = $MarginContainer/VersionLabel
+@onready var version_label = $MarginContainer/VBoxContainer2/VersionLabel
 
 func _ready() -> void:
 	if not MusicManager.is_playing():
@@ -13,7 +13,7 @@ func _ready() -> void:
 	else:
 		if not MusicManager.general_finished.is_connected(MusicManager.start_menu):
 			MusicManager.general_finished.connect(MusicManager.start_menu, Node.CONNECT_ONE_SHOT)
-	version_label.text = "v%s" % ProjectSettings.get_setting("application/config/version")
+	version_label.text = ProjectSettings.get_setting("application/config/version")
 
 
 func _on_start_button_pressed() -> void:
